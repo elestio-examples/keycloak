7c1d39ec4c24d0289e4bd0a59fbe82e483920fc4
Prem Chavhan <pchavhan@logiquad.com>
2023-06-29 20:33:09 +0530

    Updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -2,4 +2,5 @@
 rm -rf ./quarkus/container/Dockerfile
 cp -r ./quarkus/container/ ./
 release_version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/keycloak/keycloak/releases/latest | rev | cut -d "/" -f1 | rev)
-docker build --build-arg KEYCLOAK_DIST=https://github.com/keycloak/keycloak/releases/download/${release_version}/keycloak-${release_version}.tar.gz . --tag elestio4test/keycloak:latest
+sed -i "s~KEYCLOAK_VERSION~${release_version}~g" ./Dockerfile
+docker build . --tag elestio4test/keycloak:latest
