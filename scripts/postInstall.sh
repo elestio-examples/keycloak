79c795292f33dcf78404dd6959006be7eed34b28
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-11 12:55:50 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,10 @@
-#nothing to do
+set -o allexport; source .env; set +o allexport;
+
+
+echo "Waiting for redis insight to be ready ..."
+sleep 30s;
+
+docker-compose exec -T postgresql bash -c "psql -U postgres postgres <<EOF
+    \c keycloak
+    UPDATE public.user_entity SET \"email\"='${ADMIN_EMAIL}', \"email_constraint\"='${ADMIN_EMAIL}' WHERE \"username\"='root';
+EOF";
