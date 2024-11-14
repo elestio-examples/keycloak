1bdf4c6f782cb36a8eb200646ae9254e3b217100
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-11-14 17:52:27 +0200

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-rm -rf ./quarkus/container/Dockerfile
+# rm -rf ./quarkus/container/Dockerfile
 cp -r ./quarkus/container/ ./
 release_version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/keycloak/keycloak/releases/latest | rev | cut -d "/" -f1 | rev)
-sed -i "s~KEYCLOAK_VERSION~${release_version}~g" ./Dockerfile
+# sed -i "s~KEYCLOAK_VERSION~${release_version}~g" ./Dockerfile
+sed -i "s~ENV KEYCLOAK_VERSION 999.0.0-SNAPSHOT~ENV KEYCLOAK_VERSION ${release_version}~g" ./Dockerfile
+sed -i "s~ARG KEYCLOAK_VERSION~ARG KEYCLOAK_VERSION=${release_version}~g" ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/keycloak:latest | docker load
